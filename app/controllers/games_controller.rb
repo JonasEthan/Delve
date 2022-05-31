e0ac48d27a5594e8cdc44dc2cb4308ee5f547624
Lena Schneider <lena.schneider407@gmail.com>
2022-05-31 14:32:59 +0200

    games controller updated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   def create
     @game = Game.new
-    @character = Character.find(params[:character_id])
+    @character = Character.find(1) # Seeded character with ID = 1
     @game.character = @character
     @game.save
   end
--- app/models/character.rb
+++ app/models/character.rb
@@ -2,3 +2,5 @@ class Character < ApplicationRecord
   has_many :games
   has_many :users
 end
+
+# We seed 1 single character with 1 ID, that EVERY player uses.
