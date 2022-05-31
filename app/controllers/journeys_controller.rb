968c7f326abecc49dff39f2cc6b5eab0f01dd379
Lena Schneider <lena.schneider407@gmail.com>
2022-05-31 15:48:58 +0200

    characters controller added

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -4,4 +4,22 @@ class JourneysController < ApplicationController
     @run = Run.new          # run instance hier erstellt, damit im Form in journeys/index view erreichbar
   end
 
+  def menu
+    @journeys = Journey.find()
+  end
+
+  # def completed
+  # end
+
+  # def repeat
+  # end
+
+  ###
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
 
   resources :games do
-    resources :runs
+    resources :runs, only: [:create]
   end
+                      # edit & update ?
 end
