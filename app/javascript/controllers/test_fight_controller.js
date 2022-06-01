7cb2bf16d93deb6a33d3df86650d092abbaab439
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-06-01 17:26:14 +0200

    Boss does more damage now

--- app/javascript/controllers/test_fight_controller.js
+++ app/javascript/controllers/test_fight_controller.js
@@ -8,13 +8,21 @@ export default class extends Controller {
   }
 
   attackPlayer(){
-    this.playerHealthTarget.innerText -= (this.enemyValue.attack_damage + Math.floor(Math.random() * 3));
+    if(this.enemyValue.boss){
+      this.playerHealthTarget.innerText -= (this.enemyValue.attack_damage + Math.floor(Math.random() * 6));
+    }else{
+      this.playerHealthTarget.innerText -= (this.enemyValue.attack_damage + Math.floor(Math.random() * 3));
+    }
   }
 
   attackEnemy(){
     if (Number(this.enemyHealthTarget.innerText) > 0 ){
       this.enemyHealthTarget.innerText -= (this.playerValue.attack_damage + Math.floor(Math.random() * 6));
-      this.attackPlayer();
+      if (Number(this.enemyHealthTarget.innerText) > 0 ){
+        this.attackPlayer();
+      }
+    }else{
+      alert(`You have overcome ${this.enemyValue.name}`);
     }
   }
 }
