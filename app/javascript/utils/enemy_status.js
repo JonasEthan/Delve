9599cac83366bf630b42b4d849ba0f9411244487
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-06-02 17:51:29 +0200

    for future addition

--- app/javascript/utils/enemy_status.js
+++ app/javascript/utils/enemy_status.js
@@ -8,12 +8,25 @@ export default class EnemyStatus {
   }
 
   playerAttack(attackDamage) {
-    return this.health -= attackDamage;
+    if((Math.floor(Math.random() * 101)) % 10 === 0){
+      // alert("Player CRIT!");
+      return this.health -= Math.floor(attackDamage * 1.5);
+    }else{
+      return this.health -= attackDamage;
+    }
   }
 
   checkHealth(){
     if(this.health <= 0) {
       alert(`You have overcome ${this.name}`);
+    }
+  }
+
+  checkEnergy(energy){
+    if(this.energy <= energy){
+      return true;
+    }else{
+      return false;
     }
   }
 }
