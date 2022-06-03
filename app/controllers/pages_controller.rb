76fe144e03b0089b99d6392701f05cec681804ab
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-06-03 12:46:50 +0200

    now is loaded with correct enemy type

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
   end
 
   def test_fight
-    @enemy = Enemy.all.sample
+    @enemy = Enemy.find(params[:format])
     @enemy.health += rand(-10..10)
     @enemy.energy += rand(-5..5)
     @disorder = Disorder.find(@enemy.disorder_id)
