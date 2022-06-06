ddafd3c362774d9d72be4c74b0f3068c437abd48
Lukas Biehl <lukas.biehl@gmail.com>
2022-06-06 15:12:25 +0200

    adds initial sweetalerts for fight page and connects it with narrator WIP

--- app/javascript/utils/adventuring_text.js
+++ app/javascript/utils/adventuring_text.js
@@ -2,19 +2,19 @@ export default class AdventuringText {
   checkDialog(name) {
     switch(name){
       case "The Witness":
-        alert("You hear the flapping of wings in the distance closing nearer to you by the seconde. It gaze seemed always fixed on you");
+        alert("You hear the flapping of wings in the distance closing nearer to you by the second. Its gaze seems to be always fixed on you");
       break;
       case "The Stalker":
-        alert("As you walk you feel a gaze upon you seemingly from every direction. Out of the shadow steps it its eyes fixed on you. This seems to be the last hurdle of this Journey!");
+        alert("As you walk you feel a gaze upon you, seemingly from every direction. Out of the shadow steps something with its eyes fixed on you. This seems to be the last hurdle of this journey!");
       break;
       case "The Chain":
-        alert("As you carry on through the darkening surroundings you hear the ratteling of chains drawing closer and closer. Until at last out of the fabric of reality it self burst Chain trying to contain you");
+        alert("As you carry on through the darkening surroundings you hear the ratteling of chains drawing closer and closer. Until at last out of the fabric of reality it bursts trying to attack you");
       break;
       case "The Abyss":
-        alert("Despit the best effords of this place to keep you inside it. You seem to have reached the exit. But before you can leave a looming shadow grows in its ligth and cast you once more into darkness. This place wont let you leave so easily after all.");
+        alert("Despite the best efforts of this place to keep you inside it you seem to have reached the exit. But before you can leave a looming shadow grows in its light and casts you once more into darkness. This place won't let you leave so easily after all...");
       break;
       case "Hysteria":
-        alert("The surrounding Chaos seems to grow more restless and its starts to scream. As faces seem to form out nowhere. Soon infront of you floats a Orb off conjoined faces all seemingly in pain! It seems set on not letting you pass");
+        alert("The surrounding Chaos seems to grow more restless and it starts to scream as faces seem to form out nowhere. Soon infront of you floats an orb off conjoined faces all seemingly in pain! It seems determined to not let you pass.");
       break;
       case "The Prosecuter":
         alert("Needs text still");
@@ -23,7 +23,7 @@ export default class AdventuringText {
         alert("Needs text still");
       break;
       default:
-        alert("The surrounding Chaos reforms into an algamation. It is blocking your way forward");
+        alert("The surrounding Chaos reforms into an algamation. It is blocking your way forward.");
     }
   }
 }
