2231e13661bfa27626c1e0c66822680432564121
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-06-07 15:29:52 +0200

    game log looks nicer

--- app/javascript/controllers/test_fight_controller.js
+++ app/javascript/controllers/test_fight_controller.js
@@ -10,16 +10,13 @@ import { SVG } from "@svgdotjs/svg.js";
 
 // Connects to data-controller="test-fight"
 export default class extends Controller {
-  static values = { enemy: Array, player: Object, disorder: Object, special: Object, pictures: Array}
-  static targets = ["enemyName", "enemyHealthPercent", "enemyHealth", "playerHealthPercent", "playerHealth", "playerEnergyPercent", "playerEnergy", "firstPicture", "secondPicture", "gameLog"]
+  static values = { enemy: Object, player: Object, disorder: Object, special: Object }
+  static targets = ["enemyName", "enemyHealthPercent", "enemyHealth", "playerHealthPercent", "playerHealth", "playerEnergyPercent", "playerEnergy", "firstPicture", "gameLog"]
 
   connect() {
     // creates the instances of our Player and Enemy for JS with the given Object parameters
-    this.n = 0;
     this.player = new PlayerStatus(this.playerValue.health, this.playerValue.energy, this.playerValue.attack_damage);
-    this.enemy = new EnemyStatus(this.enemyValue[this.n].name, this.enemyValue[this.n].health, this.enemyValue[this.n].energy, this.enemyValue[this.n].attack_damage, this.enemyValue[this.n].boss);
-    this.picture = this.picturesValue[this.n];
-    // this.pictureDisplay(this.picture);
+    this.enemy = new EnemyStatus(this.enemyValue.name, this.enemyValue.health, this.enemyValue.energy, this.enemyValue.attack_damage, this.enemyValue.boss);
     this.narrator = new AdventuringText;
     this.gameLog = new GameLog;
     //this.narrator.checkDialog(this.enemy.name);
@@ -30,24 +27,6 @@ export default class extends Controller {
   }
 
   // when the current enemy is defeated this reassigns the new enemy
-  do() {
-    if(this.n > 0){
-      this.enemy.name = this.enemyValue[this.n].name;
-      this.enemy.health = this.enemyValue[this.n].health;
-      this.enemy.energy = this.enemyValue[this.n].energy;
-      this.enemy.damage = this.enemyValue[this.n].attack_damage;
-      this.enemy.boss = this.enemyValue[this.n].boss;
-      // reasigned to properly show the healthbar
-      this.enemyMaxhealth = this.enemy.health;
-      this.picture = this.picturesValue[this.n];
-      // The updating of the picture does not work correctly as of yet For some reason JS does not want to delete the firstChild node correctly
-      this.firstPictureTarget.classList.add("hidden");
-      this.secondPictureTarget.classList.remove("hidden");
-
-      this.updateView();
-      this.narrator.checkDialog(this.enemy.name);
-    }
-  }
 
   // Loads the svg pictures of our enemies
   pictureDisplay(picture) {
@@ -121,14 +100,8 @@ export default class extends Controller {
         }, 500);
       } else {
         // !this.fightWin()
-        // this loads the next enemy of the array
-        this.n += 1;
-        this.do();
       }
     } else {
-      // this loads the next enemy of the array
-      this.n += 1;
-      this.do();
     }
   }
 
@@ -142,8 +115,6 @@ export default class extends Controller {
         this.attackPlayer()
       }, 500);
     } else {
-      this.n += 1;
-      this.do();
     }
   }
 
@@ -154,16 +125,17 @@ export default class extends Controller {
 
   // Updates the entire HTML so that the View is dynamic
   updateView() {
-    this.enemyNameTarget.innerText = this.enemy.name
+    // Player view update
     this.playerHealthPercentTarget.innerText = `${((this.player.health * 100) / this.player.maxHealth)}%`;
     this.playerHealthTarget.value = this.player.health;
     this.playerEnergyPercentTarget.innerText = `${Math.floor((this.player.energy * 100) /this.player.maxEnergy)}%`;
     this.playerEnergyTarget.value = this.player.energy;
+    // Enemy view update
+    this.enemyNameTarget.innerText = this.enemy.name
     this.enemyHealthPercentTarget.innerText = `${Math.floor((this.enemy.health * 100) /this.enemyMaxhealth)}%`;
     this.enemyHealthTarget.max = this.enemyMaxhealth;
     this.enemyHealthTarget.value = this.enemy.health;
-      this.gameLogTarget.insertAdjacentHTML("beforeend", this.gameLogAction)
-
+    this.gameLogTarget.insertAdjacentHTML("beforeend", `<li>${this.gameLogAction}</li>`)
     // this.pictureTarget.innerText.remove();
   }
 }
