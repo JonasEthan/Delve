a960a363511bdc4b610b7ccb9a3c4adf2f1d55ce
Lukas Biehl <lukas.biehl@gmail.com>
2022-06-10 11:07:46 +0200

    adds proper styling to game log colors

--- app/javascript/utils/game_log.js
+++ app/javascript/utils/game_log.js
@@ -7,18 +7,18 @@ export default class GameLog {
         return "";
       // Player actions
       case "playerAttack":
-        return "<br><style='color:#85A9AB'>–––<br><em><span style='color:#85A9AB'>You gather your strength and strike!</em>";
+        return "<br><style='color:#85A9AB'>–––<br><em><span style='color:#85A9AB'>You gather your strength and strike!</span></em>";
       case "playerSpecial":
         return "<br><em><span style='color:#85A9AB'>–––<br>You gather your energy for a special move...</em></span>";
-    // Enemy conditions
+    // Player conditions
     case "Healing1":
-      return "<br>You collect yourself and manage to calm down <br>Some of your health is restored";
+      return "<br><style='color:#85A9AB'>–––<br><em><span style='color:#85A9AB'>You collect yourself and manage to calm down... <br>Some of your health is restored.</span>";
     case "Heavy Attack":
-      return "<br>You collect your might to strike your foe with a powerful attack!";
+      return "<br><style='color:#85A9AB'>–––<br><em><span style='color:#85A9AB'>You collect your might to strike your foe with a powerful attack!</span>";
     case "Reckless Attack1":
-      return "<br>You throw caution to the wind and strike without remorse!<br> You came out of the scuffle unscathed <span class=special-font>this time</span>";
+      return "<br><style='color:#85A9AB'>–––<br><em><span style='color:#85A9AB'>You throw caution to the wind and strike without remorse!<br> You came out of the scuffle unscathed <span class=special-font>this time</span>";
     case "Reckless Attack2":
-      return "<br>You throw caution to the wind and strike without remorse!<br>But in the scuffle you also <span class=special-font>harm yourself</span>";
+      return "<br><style='color:#85A9AB'>–––<br><em><span style='color:#85A9AB'>You throw caution to the wind and strike without remorse!<br>But in the scuffle you also <span class=special-font>harm yourself</span>";
     case "enemyAttack":
       //setTimeout(() => {
         return "<br>The opponent's dark forces drain your health!";
@@ -31,7 +31,7 @@ export default class GameLog {
     case "energyLow":
       return "<br><style='color:#42475B>–––<br><span style='color:#DE9B8C'><strong>Your energy level is too low!</strong></span>";
     case "energyLowEnemy":
-      return "<br>Your foe tries to gather their strength, but fails"
+      return "<br>Your foe tries to gather their strength, but fails..."
     // Enemy communication
     case "The Witness":
       return "<br>The Witness casts a <span class=special-font>Horrible Gaze</span> at you!<br> You feel your energy depleting...";
