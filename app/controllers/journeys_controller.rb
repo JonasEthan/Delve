7aa2ec89cb692d8b676eacffa1023c6b249de51b
Lena Schneider <lena.schneider407@gmail.com>
2022-06-10 06:52:57 +0200

    completed page with correct layout

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -50,8 +50,8 @@ class JourneysController < ApplicationController
   end
 
   def completed
-    @disorder = Disorder.find(1) # hard-coded for now, to be able to access journeys/completed directly; instead of the number it should be: params[:id]
-    @enemies = Enemy.where(disorder_id: 1) # hard-coded for now, to be able to access journeys/completed directly; instead of the number it should be: params[:id]
+    @disorder = Disorder.find(3) # hard-coded for now, to be able to access journeys/completed directly; instead of the number it should be: params[:id]
+    @enemies = Enemy.where(disorder_id: 3) # hard-coded for now, to be able to access journeys/completed directly; instead of the number it should be: params[:id]
     @enemy = @enemies.last
   end
 
